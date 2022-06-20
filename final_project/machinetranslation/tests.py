9ddad64a31ade49aa0a5f67fccfba4cdc72190bc
OhmmyD <ohm.doungkhae@gmail.com>
2022-06-20 21:49:25 +0000

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,6 +1,6 @@
 """ Unit test for the translator module """
 import unittest
-from machinetranslation import translator
+import translator
 
 #from translator import englishToFrench, double
 
@@ -8,6 +8,8 @@ class TestenglishToFrenchpyth(unittest.TestCase):
     def test1(self): 
         self.assertEqual(translator.englishToFrench('Goodbye'), 'Au revoir')
         self.assertEqual(translator.englishToFrench('Good evening'), 'Bonsoir')
+        self.assertNotEqual(translator.englishToFrench('Goodbye'), 'Goodbye')
+        self.assertNotEqual(translator.englishToFrench('Good evening'), 'Good evening')
         self.assertEqual(translator.englishToFrench(0), '0')
         self.assertEqual(translator.englishToFrench('Hello'), 'Bonjour')
 
@@ -15,6 +17,8 @@ class TestfrenchToEnglish(unittest.TestCase):
     def test1(self): 
         self.assertEqual(translator.frenchToEnglish('Au revoir'), 'Goodbye')
         self.assertEqual(translator.frenchToEnglish('Bonsoir'), 'Good evening')
+        self.assertNotEqual(translator.frenchToEnglish('Au revoir'), 'Au revoir')
+        self.assertNotEqual(translator.frenchToEnglish('Bonsoir'), 'Bonsoir')
         self.assertEqual(translator.frenchToEnglish(0), '0')
         self.assertEqual(translator.frenchToEnglish('Bonjour'), 'Hello')
 
